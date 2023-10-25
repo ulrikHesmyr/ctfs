7bf671c70d2981c67511315770e0ca1d6b27a348
ulrikHesmyr <ulrik.hesmyr2002@gmail.com>
2023-10-25 17:35:38 +0200

    helmet

--- misc/castle_approach/index.js
+++ misc/castle_approach/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const path = require('path');
+const helmet = require("helmet");
 
 const {FLAG, ENV, LOGINROUTE, PORT} = process.env;
 
@@ -40,6 +41,7 @@ async function ipValidation(req,res,next){
     
 }
 
+app.use(helmet());
 app.use(ipValidation);
 app.use(express.json());
 app.use(express.static("build"));
